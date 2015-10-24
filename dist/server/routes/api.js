119149e4240bfbcbf159831b4807b1f529408e65
Dylan Fischler <efischler@umass.edu>
2015-10-24 13:08:31 -0400

    removing dist files

--- dist/server/routes/api.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-  app.get('/api', function(req,res){
-  	res.json("Welcome to the TEDxAmherst API");
-  });
-
-  require('./watson.js')(app);
-};
--- dist/server/routes/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.render('index', { title: 'TEDxAmherst' });
-  });
-
-  require('./api.js')(app);
-};
